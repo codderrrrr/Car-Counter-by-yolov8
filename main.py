4ecf0f4f45774b5b2f7058aa3212b18c78db1041
Saad <saadwaqar118@gmail.com>
2025-01-19 15:32:59 +0500

    Counter implemented

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ mask = cv2.imread('./mask.png')
 # tracking
 track = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
 limits = [400, 297, 673, 297]
-
+totalCount = 0
 while True:
     ret, frame = cap.read()
     imgRegion = cv2.bitwise_and(frame, mask)
@@ -56,6 +56,13 @@ while True:
         print(res)
         cvzone.cornerRect(frame, (x1, y1, w, h), l=9, rt=5, colorR=(255, 0, 255))
         cvzone.putTextRect(frame, f'{int(id)}', (max(20, x1), max(0, y1)), scale=1, thickness=1)
+
+        cx, cy = x1+w//2, y1+h//2
+        cv2.circle(frame, (cx,cy), 5, (255, 0, 255), cv2.FILLED)
+
+        if limits[0] < cx < limits[2] and limits[1]-20 < cy < limits[1]+20:
+            totalCount += 1
+        cvzone.putTextRect(frame, f' Count: {totalCount}', (50, 50))
 
     cv2.imshow('Car', frame)
     cv2.waitKey(0)
